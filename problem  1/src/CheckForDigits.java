88415b13c9663d7d3d206dbd3527eddc428eae41
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-08 17:01:42 +0600

    we use recursion by calling method isAllDigits inside itself to check if it's true for all characters one by one

--- problem  1/src/CheckForDigits.java
+++ problem  1/src/CheckForDigits.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 public class CheckForDigits {
-    public static boolean isDigits(String s){
-
+    public static boolean isAllDigits(String s){
+        char c= s.charAt(0);
+        if (s.length() == 1) {
+            char ch = s.charAt(0);
+            return Character.isDigit(ch);
+        }
+        if (Character.isDigit(c)){
+            return isAllDigits(s.substring(1));
+        }else{
+            return false;
+        }
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner scan= new Scanner(System.in);
         System.out.println("Enter the string:");
         String input= scan.nextLine();
-        boolean res=isDigits(input);
-
+        boolean res=isAllDigits(input);
+        System.out.println(res ? "yes":"no");
     }
 }
-
