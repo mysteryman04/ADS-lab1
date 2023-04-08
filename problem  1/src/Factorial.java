c23f1f733ba1b5fe3745dcff6535bc209bf68060
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-08 17:38:43 +0600

    erased

--- problem  1/src/Factorial.java
+++ problem  1/src/Factorial.java
@@ -1,18 +1,1 @@
-import java.util.Scanner;
 
-public class Factorial {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a 'n' ");
-        int n = scanner.nextInt();
-
-        System.out.println(n + "! = " + factorial(n));
-    }
-    public static int factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
-    }
-}
