91ea00e008551fdc011e18105015d8e2b44abc68
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-08 15:22:40 +0600

    implemented main method

--- problem  1/src/CheckForDigits.java
+++ problem  1/src/CheckForDigits.java
@@ -1,2 +1,14 @@
 import java.util.Scanner;
+public class CheckForDigits {
+    public static boolean isDigits(String s){
+
+    }
+    public static void main(String args[]){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String input= scan.nextLine();
+        boolean res=isDigits(input);
+
+    }
+}
 
