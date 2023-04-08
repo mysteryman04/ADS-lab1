9ba2c2478904130fe8b7ffd751219f0451cdb0ad
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-08 17:58:36 +0600

    main method that takes the input and out with method result

--- problem  1/src/Factorial.java
+++ problem  1/src/Factorial.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
+public class Factorial{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int num = scan.nextInt();
+        System.out.println(factorial(num));
+    }
 
+}
