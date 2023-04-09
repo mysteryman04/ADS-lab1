f7271361902a523d08b5ad5d5e1f1db7587eab06
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-09 12:53:32 +0600

    going to change the method by solving the task with another faster algorithm

--- problem  1/src/IsPrime.java
+++ problem  1/src/IsPrime.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class IsPrime {
     public static boolean isPrime(int n, int i) {
-        if (n <= 2) {
-            return (n == 2);
-        } else if (n % i == 0) {
-            return false;
-        } else if (i * i > n) {
-            return true;
-        } else {
-            return isPrime(n, i+1);
-        }
     }
 
     public static void main(String[] args) {
