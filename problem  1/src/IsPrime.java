3c4693314db29d93b515813d5a3e158a939c2d4f
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-04-09 13:25:40 +0600

    after checking for first signs, we call method isPrimeHelper. if divisor is not appropriate we call method again by adding to divisor 2

--- problem  1/src/IsPrime.java
+++ problem  1/src/IsPrime.java
@@ -11,13 +11,22 @@ public class IsPrime {
         if(n%2==0){
             return false;
         }
-        return isPrimeHelper()
+        return isPrimeHelper(n,3);
+    }
+    public static boolean isPrimeHelper(int n,int divisor){
+        if(divisor>Math.sqrt(n)){
+            return true;
+        }
+        if(n%divisor==0){
+            return false;
+        }
+        return isPrimeHelper(n,divisor+2);
     }
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n = scan.nextInt();
-        boolean prime = isPrime(n, 2);
+        boolean prime = isPrime(n);
         if (prime) {
             System.out.println("prime");
         } else {
